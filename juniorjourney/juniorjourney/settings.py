1b69672d5dc5cf97041ff95fc7a3b3617ef4d3ba
Guy <gkomash@gmail.com>
2024-06-02 08:13:00 +0300

    User Sign In & Out. Basic Authorization. User Post Creation

--- juniorjourney/juniorjourney/settings.py
+++ juniorjourney/juniorjourney/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +25,7 @@ SECRET_KEY = 'django-insecure-exnb@ceqs6idt5qrpdkzr(ubf1)0=&ilk)=^*!!a*24)f&r0=1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -119,8 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'assets'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+  BASE_DIR / 'static'
+    ]
 
 # MEDIA_URL = 'media/'
 
--- juniorjourney/posts/views.py
+++ juniorjourney/posts/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Post
-
+from django.contrib.auth.decorators import login_required
+from . import forms
 # Create your views here.
 
+@login_required(login_url="/users/login/")
 def posts_list(request):
     posts = Post.objects.all().order_by('-date')
     return render(request, 'posts/posts_list.html',{ 'posts':posts})
 
+@login_required(login_url="/users/login/")
 def post_page(request, slug):
     post = Post.objects.get(slug=slug)
     return render(request, 'posts/post_page.html',{ 'post': post})
+
+@login_required(login_url="/users/login/")
+def post_new(request):
+    if request.method == "POST":
+        form = forms.CreatePost(request.POST)
+        if form.is_valid():
+            # save new post with user
+            newpost = form.save(commit=False)
+            newpost.author = request.user
+            newpost.save()
+            return redirect('posts:list')
+    else:
+        form = forms.CreatePost()
+    return render(request, 'posts/post_new.html', {'form': form})
