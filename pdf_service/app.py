e321043edb9b321d7006dc1492eb2e9a14ac4ab1
guykomash <gkomash@gmail.com>
2024-06-05 16:34:21 +0300

    .env in pdf service

--- pdf_service/app.py
+++ pdf_service/app.py
@@ -5,22 +5,29 @@ import threading
 import listeners
 import sys
 import signal
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+KAFKA_SERVER = os.getenv('KAFKA_SERVER')
+KAFKA_USERNAME = os.getenv('KAFKA_USERNAME')
+KAFKA_PASSWORD = os.getenv('KAFKA_PASSWORD')
 
 app = Flask(__name__)
 
 consumer = Consumer({
-'bootstrap.servers':'pkc-7rgxp.il-central-1.aws.confluent.cloud:9092',
-'security.protocol':'SASL_SSL',
-'sasl.mechanisms':'PLAIN',
-'sasl.username':'F7JAPZ6WCHJLBZ44',
-'sasl.password':'C76nZ9QAIizl7pjLYwJ+mpmqe8ru0eLUT0CwDzBuuQEy28d7FT9M50N10Gzc6Jco',
-'group.id':'notifications-group',
-'auto.offset.reset':'earliest',
+    'bootstrap.servers': KAFKA_SERVER,
+    'security.protocol':'SASL_SSL',
+    'sasl.mechanisms':'PLAIN',
+    'sasl.username': KAFKA_USERNAME,
+    'sasl.password':KAFKA_PASSWORD,
+    'group.id':'pythob-group-1',
+    'auto.offset.reset':'earliest',
 
-# Best practice for higher availability in librdkafka clients prior to 1.7
-'session.timeout.ms':'45000',
+    # Best practice for higher availability in librdkafka clients prior to 1.7
+    'session.timeout.ms':'45000',
 })
-
 
 
 def kafka_consumer():
