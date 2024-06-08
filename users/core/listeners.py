8819f8285a6628ad1da4c9e8584fe002241a1700
guykomash <gkomash@gmail.com>
2024-06-08 19:29:50 +0300

    Removed notifications and users services from repository

--- users/core/listeners.py
+++
@@ -1,8 +0,0 @@
-from django.utils.dateparse import parse_datetime
-
-def subscribe(data):
-    print(f"subscriptions service consumed: {data}")
-
-def unsubscribe(data):
-    print(f"subscriptions service consumed: {data}")
-
