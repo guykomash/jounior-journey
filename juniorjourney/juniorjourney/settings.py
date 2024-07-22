5f4ffc6c8dd1b44d4d5d2d5cdfad114f22d6b36a
guykomash <gkomash@gmail.com>
2024-07-22 21:11:04 +0300

    django db env

--- juniorjourney/juniorjourney/settings.py
+++ juniorjourney/juniorjourney/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+from dotenv import load_dotenv
 from pathlib import Path
 import os
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -85,8 +87,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'users',
-        'USER': 'root',
-        'PASSWORD': 'MYSQL1234',
+        'USER': os.getenv('MYSQL_USERNAME'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
